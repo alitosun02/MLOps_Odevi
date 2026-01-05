ab54f60116d221d758f30c439f17231c84ac2b15
Ali Tosun <02alitosun@gmail.com>
2026-01-05 04:15:54 +0300

    Bucket size mantigi degistirildi
    Bucket size mantigi degistirildi

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,7 +3,7 @@ def hash_feature(input_string, bucket_size=100):
     if not isinstance(input_string, str):
         raise ValueError("Girdi bir string olmalı")
 
-    hashed_value = hash(input_string) % bucket_size
+    hashed_value = hash(input_string) 
     return hashed_value
 
 if __name__ == "__main__":
